25fc1c0c48be2f325303c55c3446e69e430004b7
NeilsonHu <7145157+NeilsonHu@users.noreply.github.com>
2022-03-18 18:44:18 -0400

    #update add icons

--- MyMacActive/MyMacActive/AppDelegate.m
+++ MyMacActive/MyMacActive/AppDelegate.m
@@ -17,7 +17,6 @@
     // Insert code here to initialize your application
 }
 
-
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
